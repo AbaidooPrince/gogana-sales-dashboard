cf1dc96972f813f7a9aa57a0bd43e09cf7902892
nx-prince-abaidoo <prince.abaidoo@nexum.com>
2022-09-02 10:05:07 +0000

    update

---
+++ src/components/GoBarChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex gap-4">
+    <div class="grid gap-4">
+      <div v-for="scale in yAxis" :key="scale">{{ scale }}</div>
+    </div>
+    <div class="grid grid-cols-7 gap-8 w-full">
+      <div>
+        <bar-chart-bar key="9" :level="convert(80)"></bar-chart-bar>
+        <div>Sun</div>
+      </div>
+      <div>
+        <bar-chart-bar key="6" :level="convert(10)"></bar-chart-bar>
+        <div>Mon</div>
+      </div>
+      <div>
+        <bar-chart-bar key="3" :level="convert(70)"></bar-chart-bar>
+        <div>Tue</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BarChartBar from "../atoms/BarChartBar.vue";
+import { convertToBar } from "../helpers/converter";
+export default {
+  name: "GoBarChart",
+  components: { BarChartBar },
+  data() {
+    return {
+      yAxis: ["100K", "80K", "60K", "50K", "20K", "0"],
+    };
+  },
+  methods: {
+    convert(num) {
+      return convertToBar(num);
+    },
+  },
+};
+</script>
--- src/helpers/converter.js
+++ src/helpers/converter.js
@@ -5,3 +5,14 @@ export const convertToK = (num) => {
     return (num % 1000).toFixed(1);
   }
 };
+
+export const convertToBar = (num) => {
+  const toK = convertToK(num);
+  if (toK === 99) {
+    return "h-full";
+  } else if (toK > 0 && toK <= 10) {
+    return "h-4";
+  } else if (toK > 10 && toK <= 20) {
+    return "h-1/5";
+  }
+};
